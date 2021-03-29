af4dafa8db33084fd62c5c58ce96fe2f2c36e880
Paulina <paula.rachwalska@gmail.com>
2021-03-29 10:20:59 +0200

    Paula_autko_blue 0.0.9

--- Gra autka.py
+++ Gra autka.py
@@ -64,9 +64,6 @@ while running:
     # tło ekranu
     screen.fill((184, 225, 233))  # Red Green Blue 0-->255
 
-    # RUCH AUTKA KOMPUTERA
-    redX += random.randint(1, 2) /10
-    redY += 0.001
 
 
     for event in pygame.event.get():
@@ -79,10 +76,9 @@ while running:
             SpeedMetaX = -0.3
             SpeedcarX = 0.06
             SpeedMeta_Pygame = -0.3
-        if event.key == pygame.K_LEFT:
-            SpeedMetaX = 0.3
-            SpeedcarX = - 0.06
-            SpeedMeta_Pygame = 0.3
+            #RUCH AUTKA KOMPUTERA
+            Speedcar1 = 0.01
+            redY += 0.001
 
         if event.key == pygame.K_UP:
             SpeedcarY = -0.3
@@ -94,9 +90,20 @@ while running:
             SpeedMetaX = 0
             SpeedMeta_Pygame = 0
             SpeedcarX = 0
+            Speedcar1 = 0.05
         if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
             SpeedcarY = 0
             SpeedMeta_Pygame = 0
+
+
+
+
+    if redX >= 650:
+        SpeedcarX = 0
+        SpeedMeta_Pygame = 0
+        SpeedMetaX = 0
+        SpeedcarY = 0
+        Speedcar1 = 0
 
 
 
@@ -105,11 +112,18 @@ while running:
         metaX += SpeedMetaX
         blueY += SpeedcarY
         blueX += SpeedcarX
+        redX += Speedcar1
     elif blueX >= 650:
         SpeedcarX = 0
         SpeedMeta_Pygame = 0
         SpeedMetaX = 0
         SpeedcarY = 0
+        Speedcar1 = 0
+
+
+
+    print(redX)
+
 
 
 
@@ -122,7 +136,7 @@ while running:
     elif blueX >= 736:  #736
         bluerX = 736
 
-    if blueY >= 550:  # spróbuj 600
+    if blueY >= 550:  #
         blueY = 550
     elif blueY <= 250:  #536 - to do samej góry by było
         blueY = 250
